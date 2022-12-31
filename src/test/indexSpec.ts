b5661fb223e472c7ebfd927d985733830e7d09cb
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2022-12-31 12:05:54 +0100

    Final Messages

--- src/test/indexSpec.ts
+++ src/test/indexSpec.ts
@@ -1,5 +1,8 @@
 import supertest from 'supertest';
 import app from '../index';
+import { filePresence, fileDirectory, imageSize } from '../imageFunctions/imageFunction';
+import path from 'path'
+
 const request = supertest(app)
 
 describe("test the image", ()=> {
@@ -9,8 +12,24 @@ describe("test the image", ()=> {
     });
      // check for missing query parameters
      it('checks for missing query parameters', async () => {
-        const response: string | any = request.get('/images');
+        const response = await request.get('/images');
         expect(response.status).toBe(400);
       });
+
+      it('should check for valid image parameter', async () => {
+        const response = await request.get('/images?f=imageone&x=png&w=100&h=100');
+        expect(response.status).toBe(400)
+      });
+
+      it('checks file exists', async () => {
+        const inputFile = path.join(
+          __dirname,
+          '../../pictures/secret/imageone.png');
+        const fileExists = await filePresence(inputFile);
+        expect(fileExists).toBe(true);
+      });
+
 });
+
+
 
